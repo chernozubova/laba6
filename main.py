9eae360f0a8ccdaafe11d943bd4b8c79042cd925
chernozubova <chenolika@mail.ru>
2023-12-27 16:48:32 +0700

    first stage

---
+++ main.py
@@ -0,0 +1,73 @@
+class Plane:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+
+class Flight:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+
+class Ticket:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        return
+
+
+def create_plane():
+    return
+
+
+def create_flight():
+    return
+
+
+def create_ticket():
+    return
+
+
+def menu():
+    plane = None
+    flight = None
+    ticket = None
+
+    while True:
+        print("\nМеню:")
+        print("1. Создать самолет")
+        print("2. Создать рейс")
+        print("3. Создать билет")
+        print("4. Вывести информацию о самолете")
+        print("5. Вывести информацию о рейсе")
+        print("6. Вывести информацию о билете")
+        print("7. Выход из программы")
+
+        point = input("Выберите пункт меню: ")
+
+        if point == "1":
+            pass
+        elif point == "2":
+            pass
+        elif point == "3":
+            pass
+        elif point == "4":
+            pass
+        elif point == "5":
+            pass
+        elif point == "6":
+            pass
+        elif point == "7":
+            pass
+        else:
+            pass
+
+
+if __name__ == "main":
+    menu()
