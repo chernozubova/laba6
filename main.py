521e824affca447c8e27828da0922bffa9b3894b
chernozubova <chenolika@mail.ru>
2023-12-28 12:09:00 +0700

    information

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 class Plane:
     def __init__(self, name):
+        """Инициализация атрибутов класса"""
         self.__name = name
         self.__flights = []
 
     @property
     def name(self):
+        """Уровень доступности атрибута"""
         return self.__name
 
     @property
     def flights(self):
+        """Уровень доступности атрибута"""
         return self.__flights
 
     def __str__(self):
+        "Вывод информации об объекте"
         return f"Самолет авиакомпании: {self.name}"
 
     def add_flight(self, flight):
+        "Добавляет новый рейс в список"
         self.__flights.append(flight)
 
 
 class Flight:
     def __init__(self, origin, destination, plane):
+        """Инициализация атрибутов класса"""
         self.__origin = origin
         self.__destination = destination
         self.__plane = plane
@@ -27,66 +33,77 @@ class Flight:
 
     @property
     def origin(self):
+        """Уровень доступности атрибута name"""
         return self.__origin
 
     @property
     def destination(self):
+        """Уровень доступности атрибута"""
         return self.__destination
 
     @property
     def plane(self):
+        """Уровень доступности атрибута"""
         return self.__plane
 
     @property
     def tickets(self):
+        """Уровень доступности атрибута"""
         return self.__tickets
 
     def __str__(self):
+        "Вывод информации об объекте"
         return f"Рейс из {self.origin} в {self.destination} на самолете авиакомпании {self.plane.name}"
 
     def add_ticket(self, ticket):
+        "Добавляет новый билет в список"
         self.__tickets.append(ticket)
 
 
 class Ticket:
     def __init__(self, passenger, seat, flight):
+        """Инициализация атрибутов класса"""
         self.__passenger = passenger
         self.__seat = seat
         self.__flight = flight
 
     @property
     def passenger(self):
+        """Уровень доступности атрибута"""
         return self.__passenger
 
     @property
     def seat(self):
+        """Уровень доступности атрибута"""
         return self.__seat
 
     @property
     def flight(self):
+        """Уровень доступности атрибута"""
         return self.__flight
 
     def __str__(self):
+        "Вывод информации об объекте"
         return f"Билет - Пассажир: {self.passenger} Место: {self.seat}"
 
-
 def create_plane():
+    "Создаёт авиакомпанию, запрашивая названия у пользователя"
     name = input("Введите авиакомпанию самолета: ")
     return Plane(name)
 
 
 def create_flight(plane):
+    "Создаёт рейс, запрашивая пункт отправления и пункт назначения у пользователя"
     origin = input("Введите пункт отправления: ")
     destination = input("Введите пункт назначения: ")
     return Flight(plane, origin, destination)
 
 
-def create_ticket(plane):
-    origin = input("Введите пункт отправления: ")
-    destination = input("Введите пункт назначения: ")
-    return Flight(plane, origin, destination)
-
-
+def create_ticket(flight):
+    "Создаёт билет, запрашивая имя пассажира и место пассажира у пользователя"
+    passenger = input("Введите имя пассажира: ")
+    seat = input("Введите место: ")
+    return Ticket(passenger, seat, flight)
 def menu():
     plane = None
     flight = None
@@ -141,5 +158,5 @@ def menu():
             print("Выберите верный пункт меню")
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     menu()
